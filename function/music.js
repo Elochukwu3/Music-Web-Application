9569328d18d2754b58b7f433a068233d3bd6e8e6
Elochukwu3 <oguajuromauld5@gmail.com>
2022-10-12 00:57:06 +0100

    saved function

--- function/music.js
+++ function/music.js
@@ -174,6 +174,6 @@ let kk =  playList.filter(m=>{
  musicRange.addEventListener("change", (e)=>{  
       if (isPlaying === true) {
          positon = audioTrack.currentTime * (100 / audioTrack.duration);
-         musicRange.value = positon;
+         // musicRange.value = positon;
      
     })
